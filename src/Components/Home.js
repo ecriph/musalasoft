0a370531fd01b1aab09e37f7236b8b76c6030207
ecriph <ecriphlimited@gmail.com>
2022-04-29 12:42:12 +0100

    adjusted the header

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,9 +13,10 @@ function Home() {
  <Container  className="btn-home"  >
   <Row >
     <Col sm={12}>
-      <h4>Welcome to Musalasoft Gateway System</h4>
       <div style={{display:'flex', 
       justifyContent:'center', alignItems:'center'}}>
+              <h4>Welcome to Musalasoft Gateway System</h4>
+
     <Link to="/createg">    
     <Button  className='btn-create' >
       Create a Gateway
